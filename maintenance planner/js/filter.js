e83dcf0583ae0a64148ed3123c0c3a624cac1e18
majalindved <60427034+majalindved@users.noreply.github.com>
2020-05-23 13:36:52 +0200

    click ooutside box funktion fjernet

--- maintenance planner/js/filter.js
+++ maintenance planner/js/filter.js
@@ -4,16 +4,3 @@ function filter() {
   document.getElementById("dropdown-filter").classList.toggle("show-filter-content");
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.getElementsByClassName("dropdown-filter-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show-filter-content')) {
-        openDropdown.classList.remove('show-filter-content');
-      }
-    }
-  }
-}
