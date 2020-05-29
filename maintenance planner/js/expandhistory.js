d058d072b4f8450c1c059cda4dc6927445901a1e
majalindved <60427034+majalindved@users.noreply.github.com>
2020-05-29 02:07:47 +0200

    Expand virker igen

--- maintenance planner/js/expandhistory.js
+++ maintenance planner/js/expandhistory.js
@@ -1,9 +1,9 @@
 // Expand line 1:
 
-let divs = document.getElementsByTagName('div');
+var divs = document.getElementsByTagName('div');
 
-let toggle = function() {    
-    for (let i = 0, l = divs.length; i < l; i++) {
+var toggle = function() {    
+    for (var i = 0, l = divs.length; i < l; i++) {
         if (divs[i].getAttribute('class') == 'expand1') 
             if (divs[i].style.display == 'block') divs[i].style.display = '';
             else divs[i].style.display = 'block';
@@ -14,10 +14,10 @@ document.getElementById('Toggle1').onclick = toggle;
 
 // Expand line 2:
 
-let divs = document.getElementsByTagName('div');
+var divs = document.getElementsByTagName('div');
 
-let toggle = function() {    
-    for (let i = 0, l = divs.length; i < l; i++) {
+var toggle = function() {    
+    for (var i = 0, l = divs.length; i < l; i++) {
         if (divs[i].getAttribute('class') == 'expand2') 
             if (divs[i].style.display == 'block') divs[i].style.display = '';
             else divs[i].style.display = 'block';
@@ -28,10 +28,10 @@ document.getElementById('Toggle2').onclick = toggle;
 
 // Expand line 3:
 
-let divs = document.getElementsByTagName('div');
+var divs = document.getElementsByTagName('div');
 
-let toggle = function() {    
-    for (let i = 0, l = divs.length; i < l; i++) {
+var toggle = function() {    
+    for (var i = 0, l = divs.length; i < l; i++) {
         if (divs[i].getAttribute('class') == 'expand3') 
             if (divs[i].style.display == 'block') divs[i].style.display = '';
             else divs[i].style.display = 'block';
@@ -42,10 +42,10 @@ document.getElementById('Toggle3').onclick = toggle;
 
 // Expand line 4:
 
-let divs = document.getElementsByTagName('div');
+var divs = document.getElementsByTagName('div');
 
-let toggle = function() {    
-    for (let i = 0, l = divs.length; i < l; i++) {
+var toggle = function() {    
+    for (var i = 0, l = divs.length; i < l; i++) {
         if (divs[i].getAttribute('class') == 'expand4') 
             if (divs[i].style.display == 'block') divs[i].style.display = '';
             else divs[i].style.display = 'block';
@@ -56,10 +56,10 @@ document.getElementById('Toggle4').onclick = toggle;
 
 // Expand line 5:
 
-let divs = document.getElementsByTagName('div');
+var divs = document.getElementsByTagName('div');
 
-let toggle = function() {    
-    for (let i = 0, l = divs.length; i < l; i++) {
+var toggle = function() {    
+    for (var i = 0, l = divs.length; i < l; i++) {
         if (divs[i].getAttribute('class') == 'expand5') 
             if (divs[i].style.display == 'block') divs[i].style.display = '';
             else divs[i].style.display = 'block';
